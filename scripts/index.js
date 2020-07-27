f3e62627476c3c40b64ee840148c46848b14a4dd
Igor <efimov.036@gmail.com>
2020-07-27 12:32:12 +0700

    added tabs changer

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+'use strict';
+import {
+  radioPlayerInit
+} from './radioPlayer.js';
+
+//объявление элементов страницы
+const playerBtn = document.querySelectorAll('.player-btn'),
+  playerBlock = document.querySelectorAll('.player-block'),
+  temp = document.querySelector('.temp');
+
+//функция отключения плеера
+const playerDeactivation = () => {
+  temp.style.display = 'none';
+  playerBtn.forEach(item =>
+    item.classList.remove('active')
+  );
+  playerBlock.forEach(item =>
+    item.classList.remove('active')
+  );
+};
+
+//слушатель для каждой кнопки
+playerBtn.forEach((btn, i) => btn.addEventListener('click', () => {
+  playerDeactivation();
+  btn.classList.add('active');
+  playerBlock[i].classList.add('active');
+}));
+
+radioPlayerInit();
