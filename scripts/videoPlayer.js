d9f4a81506e31298f5bef8f1769ec2ed546d49cd
Igor <efimov.036@gmail.com>
2020-07-28 12:13:20 +0700

    added stop functionality

--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -1,20 +1,44 @@
  'use strict';
 
- export const videoPlayerInit = () => {
+export const videoPlayerInit = () => {
    //объявление всех элементов плеера
-   const videoPlayer = document.querySelector('.video-player'),
-     videoButtonPlay = document.querySelector('.video-button__play'),
-     videoButtonStop = document.querySelector('.video-button__stop'),
-     videoTimePassed = document.querySelector('.vitop-time__passed'),
-     videoProgress = document.querySelector('.video-progress'),
-     videoTimeTotal = document.querySelector('.video-time__total');
+    const videoPlayer = document.querySelector('.video-player'),
+      videoButtonPlay = document.querySelector('.video-button__play'),
+      videoButtonStop = document.querySelector('.video-button__stop'),
+      videoTimePassed = document.querySelector('.vitop-time__passed'),
+      videoProgress = document.querySelector('.video-progress'),
+      videoTimeTotal = document.querySelector('.video-time__total');
 
-   //слушатель нажатия для включения воспроизведения
-   videoPlayer.addEventListener('click', () => {
-    if(videoPlayer.paused){
-      videoPlayer.play();
-    }else{
-      videoPlayer.pause();
+    //функция переключения классов у кнопки воспроизведения
+    const toggleIcon = () => {
+      if(videoPlayer.paused){
+        videoButtonPlay.classList.remove('fa-pause');
+        videoButtonPlay.classList.add('fa-play');
+      }else{
+        videoButtonPlay.classList.add('fa-pause');
+        videoButtonPlay.classList.remove('fa-play');
+      }
     }
-  });
+
+    //функция включения/выключения воспроизведения
+    const togglePlayer = () => {
+      if (videoPlayer.paused) {
+        videoPlayer.play();
+      } else {
+        videoPlayer.pause();
+      }
+      toggleIcon();
+    }
+
+    //функция остановки воспроизведения
+    const playerStop = () => {
+      videoPlayer.pause();
+      videoPlayer.currentTime = 0;
+    }
+    //слушатель нажатия для включения воспроизведения и переключения иконки
+    videoPlayer.addEventListener('click', togglePlayer);
+    videoButtonPlay.addEventListener('click', togglePlayer);
+
+    //слушатель для остановки воспроизведения
+    videoButtonStop.addEventListener('click', playerStop)
 };
