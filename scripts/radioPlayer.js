6ef787c05787db2b9fb71f3d2717437862d53a68
Igor <efimov.036@gmail.com>
2020-07-29 16:11:10 +0700

    done radio section

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -4,8 +4,8 @@ export const radioPlayerInit = () => {
   const radio = document.querySelector('.radio'),
     radioNavigation = document.querySelector('.radio-navigation'),
     radioCoverImg = document.querySelector('.radio-cover__img'),
-    radioItem = document.querySelector('.radio-item'),
-    radioHeader = document.querySelectorAll('.radio-header'),
+    radioItem = document.querySelectorAll('.radio-item'),
+    radioHeaderBig = document.querySelector('.radio-header__big'),
     radioStop = document.querySelector('.radio-stop');
 
   //создание новго объекта аудио
@@ -14,32 +14,49 @@ export const radioPlayerInit = () => {
 
   //функция переключения иконки
   const toggleRadioIcon = () => {
-    if(audio.paused){
+    if (audio.paused) {
+      radio.classList.remove('play');
       radioStop.classList.add('fa-play');
       radioStop.classList.remove('fa-stop');
-    }else{
+    } else {
+      radio.classList.add('play');
       radioStop.classList.remove('fa-play');
       radioStop.classList.add('fa-stop');
     }
   };
 
+  //функция выделения играющей станции
+  const selectItem = (elem) => {
+    radioItem.forEach(item => item.classList.remove('select'));
+    elem.classList.add('select');
+  }
   //блокировка кнопки
-  radioStop.disabled = true; 
+  radioStop.disabled = true;
 
   //слушатель изменения станции
   radioNavigation.addEventListener('change', evt => {
     const target = evt.target;
+    const parent = target.closest('.radio-item');
+    selectItem(parent);
+
+    const title = parent.querySelector('.radio-name').textContent;
+    radioHeaderBig.textContent = title;
+
+    const img = parent.querySelector('.radio-img').src;
+    radioCoverImg.src = img;
+
     radioStop.disabled = false;
     audio.src = target.dataset.radioStantion;
+
     audio.play();
     toggleRadioIcon();
   });
 
   //слушатель остановки стриминга
   radioStop.addEventListener('click', () => {
-    if(audio.paused){
+    if (audio.paused) {
       audio.play();
-    }else{
+    } else {
       audio.pause();
     }
     toggleRadioIcon();
