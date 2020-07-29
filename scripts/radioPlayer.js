239fe351724e3df96634e288247bf352391f52db
Igor <efimov.036@gmail.com>
2020-07-29 15:50:54 +0700

    added play button switching

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -12,13 +12,36 @@ export const radioPlayerInit = () => {
   const audio = new Audio();
   audio.type = 'audio/aac';
 
+  //функция переключения иконки
+  const toggleRadioIcon = () => {
+    if(audio.paused){
+      radioStop.classList.add('fa-play');
+      radioStop.classList.remove('fa-stop');
+    }else{
+      radioStop.classList.remove('fa-play');
+      radioStop.classList.add('fa-stop');
+    }
+  };
+
   //блокировка кнопки
   radioStop.disabled = true; 
 
   //слушатель изменения станции
   radioNavigation.addEventListener('change', evt => {
     const target = evt.target;
+    radioStop.disabled = false;
     audio.src = target.dataset.radioStantion;
     audio.play();
+    toggleRadioIcon();
+  });
+
+  //слушатель остановки стриминга
+  radioStop.addEventListener('click', () => {
+    if(audio.paused){
+      audio.play();
+    }else{
+      audio.pause();
+    }
+    toggleRadioIcon();
   })
 };
