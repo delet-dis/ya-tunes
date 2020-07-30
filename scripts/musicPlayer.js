70c4b6b14a327a224fcaa4310cbc2098279c7cf5
Igor <efimov.036@gmail.com>
2020-07-30 12:30:13 +0700

    added basic player code

--- scripts/musicPlayer.js
+++ scripts/musicPlayer.js
@@ -1,4 +1,31 @@
 'use strict';
 export const musicPlayerInit = () => {
+  //объявление элементов страницы
+  const audio = document.querySelector('.audio'),
+    audioImg = document.querySelector('.autio-img'),
+    audioHeader = document.querySelector('.audio-header'),
+    audioPlayer = document.querySelector('.audio-player'),
+    audioNavigation = document.querySelector('.audio-navigation'),
+    audioButtonPlay = document.querySelector('.autio-button__play'),
+    audioProgress = document.querySelector('.audio-progress'),
+    audioProgressTiming = document.querySelector('.audio-progress__timing'),
+    audioTimePassed = document.querySelector('.audio-time__passed'),
+    audioTimeTotal = document.querySelector('.audio-time__total');
 
+  //создание плейлиста
+    const playlist = ['hello', 'flow', 'speed'];
+
+  //трек, с которого будет начинаться воспроизведение
+    let trackIndex = 0;
+
+  //слушатель нажания на навигацию
+    audioNavigation.addEventListener('click', evt => {
+      const target = evt.target;
+
+      if(target.classList.contains('audio-button__play')){
+        audio.classList.toggle('play');
+        audioButtonPlay.classList.toggle('fa-play');
+        audioButtonPlay.classList.toggle('fa-pause');
+      }
+    });
 };
